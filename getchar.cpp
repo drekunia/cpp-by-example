8a35881f46acabc36cd1469f283c9ac48407414f
Andre Kurniawan <drekunia@gmail.com>
2025-12-04 20:42:19 +0700

    logical operator with getchar

--- getchar.cpp
+++ getchar.cpp
@@ -1,17 +1,30 @@
 #include <cctype>
 #include <cstdio>
 #include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[]) {
-  std::cout << "Do you want to play a game? (y/n) ";
+  cout << "Do you want to play a game? (y/n) ";
   int response = getchar();
-  std::cout << "You entered: " << static_cast<char>(response) << std::endl;
+  cout << "You entered: " << static_cast<char>(response) << endl;
   if (tolower(response) == 'y') {
-    std::cout << "Let's play a game, then...\n";
+    cout << "Let's play a game, then...\n";
+    cout << "What difficulty? (easy/medium/hard/impossible): ";
+    string difficulty;
+    cin >> difficulty;
+
+    if (difficulty == "easy" || difficulty == "medium" ||
+        difficulty == "hard") {
+      cout << "Respawn enabled.\n";
+    } else if (difficulty == "impossible") {
+      cout << "Good luck.\n";
+    } else {
+      cout << "Just play.\n";
+    }
   } else if (tolower(response) == 'n') {
-    std::cout << "Okay, we don't play anything, then...\n";
+    cout << "Okay, we don't play anything, then...\n";
   } else {
-    std::cout << "You should input \"y\" or \"n\".\n";
+    cout << "You should input \"y\" or \"n\".\n";
   }
   return 0;
 }
